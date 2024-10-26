312879f43505e7a3b48f726e80f5962d1b93f7ad
aissa-dev1 <aissabedr74@gmail.com>
2024-10-26 19:24:34 +0100

    feat: update navbar look and change its backgound color and moving to home screen when clicking the logo

--- src/components/reusable/nav-bar.tsx
+++ src/components/reusable/nav-bar.tsx
@@ -2,6 +2,7 @@ import { ComponentProps, splitProps } from "solid-js";
 import { cn } from "~/utils/cn";
 import Container from "./container";
 import ColorSchemeCard from "./color-scheme-card";
+import { A } from "@solidjs/router";
 
 interface NavBarProps extends Omit<ComponentProps<"div">, "children"> {}
 
@@ -11,17 +12,19 @@ export default function NavBar(props: NavBarProps) {
   return (
     <div
       class={cn(
-        "fixed top-0 left-0 w-full flex flex-col justify-center h-16 shadow-sm shadow-black/10 bg-opacity-25 backdrop-blur-lg backdrop-filter z-10 dark:shadow-white/10",
+        "fixed top-0 left-0 w-full flex flex-col justify-center h-16 bg-background shadow-sm shadow-black/10 z-10 dark:shadow-white/10",
         props.class
       )}
       {...rest}
     >
       <Container class="flex items-center justify-between">
-        <img
-          class="w-12 h-12 rounded-full"
-          src="/local-excellence.png"
-          alt="Local Excellence"
-        />
+        <A href="/">
+          <img
+            class="w-12 h-12 rounded-full"
+            src="/local-excellence.png"
+            alt="Local Excellence"
+          />
+        </A>
         <ColorSchemeCard />
       </Container>
     </div>
